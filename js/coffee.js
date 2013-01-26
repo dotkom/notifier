8d8a56e4752411eaa93deaf9c1ea46756eb82bb4
Michael Johansen <michael@informatikk.org>
2013-01-27 00:53:03 +0100

    Buffing up coffee.js with ajax, time calculation and more

--- js/coffee.js
+++ js/coffee.js
@@ -1,5 +1,5 @@
 var Coffee = {
-  MSG_ERROR: 'Klarte ikke finne statusen til kaffekanna',
+  msgConnectionError: 'Frakoblet fra kaffeknappen',
 
   get: function(callback) {
     if (callback == undefined) {
@@ -7,20 +7,49 @@ var Coffee = {
       return;
     }
 
-    callback('5', '0:56');
+    // Receives the status for the coffee pot
+    var self = this;
+    $.ajax({
+      url: 'http://draug.online.ntnu.no/coffee.txt',
+      success: function(data) {
 
-    // // Receives the meeting plan for today
-    // var self = this;
-    // $.ajax({
-    //   url: 'https://online.ntnu.no/service_static/online_notifier2',
-    //   success: function(data) {
-    //     callback(data);
-    //   },
-    //   error: function(jqXHR, text, err) {
-    //     if (DEBUG) console.log('ERROR: Failed to get coffee pot status.');
-    //     callback(self.MSG_ERROR);
-    //   },
-    // });
+        // Split into pot number and age of last pot
+        var pieces = data.split(/:(.+)/);
+        var pots = pieces[0];
+        var age = pieces[1];
+
+        // Calulate the age of the last pot better
+        var time = String(age.match(/\d+:\d+:\d+/)).split(':');
+        var now = new Date();
+        var then = new Date(now.getFullYear(), now.getMonth(), now.getDate(), time[0], time[1]);
+        var one_minute = 1000 * 60;
+        // Calculate difference between the two dates, and convert to minutes
+        var diff = Math.floor(( then.getTime() - now.getTime() ) / one_minute);
+        // Create a proper time string from all the minutes
+        var ageString;
+        if (-1 <= diff && diff <= 0) {
+          ageString = 'Kaffen ble <b>nettopp laget</b>';
+        }
+        else if (1 <= diff && diff <= 59) {
+          ageString = 'Kaffen ble laget for <b>'+diff+' min</b> siden';
+        }
+        else if (60 <= diff) {
+          ageString = 'Kaffen er <b>'+time[0]+':'+time[1]+'</b> gammel';
+        }
+
+        // Add 'kanner' to pots-string
+        pots = '<b>'+(pots=='0'?'Ingen kanner':pots=='1'?'1 kanne':pots+' kanner')+'</b>';
+
+        // Make a string worthy of returning
+        var coffee = ageString+'.<br />'+pots+' i dag.'
+
+        callback(coffee);
+      },
+      error: function(jqXHR, text, err) {
+        if (DEBUG) console.log('ERROR: Failed to get coffee pot status.');
+        callback(self.msgConnectionError);
+      },
+    });
   },
 
 }
