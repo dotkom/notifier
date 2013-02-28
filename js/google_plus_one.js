ef2d538cb6944bbe944b5b79e85e2521053dcf47
Michael Johansen <michael@informatikk.org>
2013-02-28 09:41:41 +0100

    Making sure plus-one button only shows up in Chrome, wontfix in Opera

--- js/google_plus_one.js
+++ js/google_plus_one.js
@@ -1,13 +1,19 @@
-window.___gcfg = {lang: 'no'};
+if (BROWSER == "Chrome") {
+  window.___gcfg = {lang: 'no'};
 
-(function() {
-	
-	var po = document.createElement('script');
-	po.type = 'text/javascript';
-	po.async = true;
-	po.src = 'https://apis.google.com/js/plusone.js';
-	
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(po, s);
-	
-})();
+  (function() {
+    
+    var po = document.createElement('script');
+    po.type = 'text/javascript';
+    po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(po, s);
+    
+  })();
+}
+else if (BROWSER == "Opera") {
+  // Wontfix for Opera 12.40, bad inline CSS bugs
+  console.log("ERROR: Google +1 button not working in Opera 12.40 because of bad inline CSS bugs");
+}
