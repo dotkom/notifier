2fb9cbf5fe1b0cf26a04fcb3181bbe78f63550cb
Michael Johansen <michael@informatikk.org>
2013-03-01 20:18:00 +0100

    Updating coffee representation, removing unused code

--- js/coffee.js
+++ js/coffee.js
@@ -1,8 +1,12 @@
 var Coffee = {
   api: 'http://draug.online.ntnu.no/coffee.txt',
+  msgNoPots: 'Ingen kanner i dag',
+  msgNoCoffee: 'Kaffen har ikke blitt satt på',
+  msgFormatError: 'Feil i kaffeformat',
   msgConnectionError: 'Frakoblet fra kaffeknappen',
-  debugCoffee: 0,
-  debugCoffeeString: "200\n28. February 22:28:371",
+  msgComforting: 'Det er sikkert kaffe!',
+  debugCoffee: 1,
+  debugCoffeeString: "200\n1. March 14:28:371",
 
   get: function(callback) {
     if (callback == undefined) {
@@ -25,17 +29,17 @@ var Coffee = {
           // Split into pot number and age of last pot
           var pieces = data.split("\n");
           var pots = pieces[0];
-          var age = pieces[1];
+          var ageString = pieces[1];
 
           // Get now
           var now = new Date();
 
           // Check if the coffee pots were made today
-          var coffeeDate = new Date(age.match(/\d+\. [a-zA-Z]+/));
+          var coffeeDate = new Date(ageString.match(/\d+\. [a-zA-Z]+/));
           if (coffeeDate.getDate() == now.getDate()) {
 
             // Calulate the age of the last pot better
-            var coffeeTime = String(age.match(/\d+:\d+:\d+/)).split(':');
+            var coffeeTime = String(ageString.match(/\d+:\d+:\d+/)).split(':');
             var then = new Date(now.getFullYear(), now.getMonth(), now.getDate(), coffeeTime[0], coffeeTime[1]);
             var one_minute = 1000 * 60;
 
@@ -43,69 +47,45 @@ var Coffee = {
             var diff = Math.floor(( now.getTime() - then.getTime() ) / one_minute);
             
             // Create a proper time string from all the minutes
-            var ageString;
+            var age;
             if (-1 <= diff && diff <= 9) {
-              ageString = 'Kaffen ble <b>nettopp laget</b>';
+              age = 'Kaffen ble <b>nettopp laget</b>';
             }
             else if (10 <= diff && diff <= 59) {
-              ageString = 'Kaffen ble laget for '+diff+' min siden';
+              age = 'Kaffen ble laget for '+diff+' min siden';
             }
             else if (60 <= diff) {
-              ageString = 'Kaffen ble laget kl '+coffeeTime[0]+':'+coffeeTime[1];
+              age = 'Kaffen ble laget kl '+coffeeTime[0]+':'+coffeeTime[1];
             }
             else {
-              ageString = 'Har noen laget kaffe i fremtiden?!'
+              age = 'Har noen laget kaffe i fremtiden?!'
             }
 
-            // Add 'kanner' to pots-string
+            // Make pots-string
             pots = (pots=='0'?'Ingen kanner':pots=='1'?'1 kanne':pots+' kanner') + ' i dag';
 
-            // Make a complete string
-            var coffee = ageString+'<br />'+pots
-            callback(coffee);
+            // Call it back
+            callback(pots, age);
           }
           else {
             // Coffee was not made today
-            var coffee = 'Kaffen har ikke blitt satt på<br />Ingen kanner i dag';
-            callback(coffee);
+            var coffee = '';
+            callback(self.msgNoPots, self.msgNoCoffee);
           }
         } catch (err) {
           // Coffee format is wrong
-          var coffee = 'Feil i kaffeformat...<br />Det er sikkert kaffe!';
-          callback(coffee);
+          callback(self.msgFormatError, self.msgComforting);
         }
       },
       error: function(jqXHR, text, err) {
         if (DEBUG) console.log('ERROR: Failed to get coffee pot status.');
-        callback(self.msgConnectionError);
-      },
-    });
-  },
-
-  getPots: function(callback) {
-    if (callback == undefined) {
-      console.log('ERROR: Callback is required. In the callback you should insert the results into the DOM.');
-      return;
-    }
-    var self = this;
-    $.ajax({
-      url: self.api,
-      success: function(data) {
-        // Find amount of pots today, convert to number
-        var pieces = data.split(/:(.+)/);
-        var pots = Number(pieces[0]);
-        callback(pots);
-      },
-      error: function(jqXHR, text, err) {
-        if (DEBUG) console.log('ERROR: Failed to get coffee pot status.');
-        callback(-1);
+        callback(self.msgConnectionError, self.msgComforting);
       },
     });
   },
 
   showNotification: function(pots) {
-    var notification = webkitNotifications.createHTMLNotification('subscription.html');
-    notification.show(); // HTML5-style
+    Browser.createNotification('subscription.html');
   },
 
 }
