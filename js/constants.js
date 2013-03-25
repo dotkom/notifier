025ba67023098860a80d0e3e96b0d2c0f37e8b04
Michael Johansen <michael@informatikk.org>
2013-03-25 03:59:26 +0100

    Removing old constants

--- js/constants.js
+++ js/constants.js
@@ -7,10 +7,10 @@ var EXTENSION_WEBSITE = 'https://online.ntnu.no';
 var CREATOR_NAME = 'dotKom'; // max 8 letters because of styling
 var CREATOR_EMAIL = 'dotkom@online.ntnu.no';
 
-// Online News API
-var API_KEY = 'f5be90e5ec1d2d454ae9';
-var API_ADDRESS = 'https://online.ntnu.no/api/' + API_KEY + '/news_image_by_id/';
-var BACKUP_IMAGE = 'img/logo-sponsor-placeholder.png';
+// // Online News API
+// var API_KEY = 'f5be90e5ec1d2d454ae9';
+// var API_ADDRESS = 'https://online.ntnu.no/api/' + API_KEY + '/news_image_by_id/';
+// var BACKUP_IMAGE = 'img/logo-sponsor-placeholder.png';
 
 // AJAX and JSON timeout
 var AJAX_TIMEOUT = 5000;
