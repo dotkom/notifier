9f70ab9bfe39896e496a9067dc3f16d30fb6fb13
Michael Johansen <michael@informatikk.org>
2013-03-26 22:47:48 +0100

    coffee: switching to dev API

--- js/coffee.js
+++ js/coffee.js
@@ -1,5 +1,5 @@
 var Coffee = {
-  api: 'http://draug.online.ntnu.no/coffee.txt',
+  api: 'http://draug.online.ntnu.no/coffee_dev.txt',
   msgNoPots: 'Ingen kanner i dag',
   msgNoCoffee: 'Kaffen har ikke blitt satt på',
   msgFormatError: 'Feil i kaffeformat',
