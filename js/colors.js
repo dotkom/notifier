5ca4c357b4120de42ebcf20274922e6c7454b296
Michael Johansen <michael@informatikk.org>
2013-03-30 01:53:48 +0100

    news: extracting affiliation and colors to separate files

---
+++ js/colors.js
@@ -0,0 +1,24 @@
+var Colors = {
+  
+  // CSS to add to #background-div
+  background: {
+    'blue':     {'-webkit-filter': 'hue-rotate(0deg)'},
+    'cyan':     {'-webkit-filter': 'hue-rotate(-31deg)'},
+    'pink':     {'-webkit-filter': 'hue-rotate(92deg)'},
+    'red':      {'-webkit-filter': 'hue-rotate(144deg)'},
+    'purple':   {'-webkit-filter': 'hue-rotate(66deg)'},
+    'green':    {'-webkit-filter': 'hue-rotate(-85deg)'},
+    'grey':     {'-webkit-filter': 'grayscale(1) hue-rotate(0deg)'},
+    'yellow':   {'-webkit-filter': 'hue-rotate(-151deg) saturate(1.6) brightness(2.3)'},
+  },
+
+  // CSS to add to #bus-div
+  bus: {
+    'blue':     {'': ''},
+  },
+
+  // CSS to add to titles
+  title {
+    'blue':     {'': ''},
+  },
+}
