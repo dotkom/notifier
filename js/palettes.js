f11da27f6fb1d0f0b65fe2d53a4f43c511275732
Michael Johansen <michael@informatikk.org>
2013-04-17 22:46:12 +0200

    palettes: adding yellow palette

--- js/palettes.js
+++ js/palettes.js
@@ -4,23 +4,20 @@ var Palettes = {
   background: {
     'pink':     {'-webkit-filter': 'hue-rotate(92deg)'},
     'purple':   {'-webkit-filter': 'hue-rotate(66deg)'},
-    'green':    {'-webkit-filter': 'hue-rotate(-85deg)'},
-    'grey':     {'-webkit-filter': 'grayscale(1) hue-rotate(0deg)'},
-    'yellow':   {'-webkit-filter': 'hue-rotate(-151deg) saturate(1.6) brightness(2.3)'},
   },
   */
 
   palettes: {
     'blue': 'less/palettes/blue.css',
-    'white': 'less/palettes/white.css',
     'cyan': 'less/palettes/cyan.css',
-    
-    'pink': 'less/palettes/pink.css',
-    'red': 'less/palettes/red.css',
-    'purple': 'less/palettes/purple.css',
     'green': 'less/palettes/green.css',
     'grey': 'less/palettes/grey.css',
+    'red': 'less/palettes/red.css',
+    'white': 'less/palettes/white.css',
     'yellow': 'less/palettes/yellow.css',
+    
+    'pink': 'less/palettes/pink.css',
+    'purple': 'less/palettes/purple.css',
   },
 
   getColor: function(color) {
