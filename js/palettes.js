58e6d49cccc673930fd9db5a394e10d58acbe1d5
Michael Johansen <michael@informatikk.org>
2013-04-21 15:36:20 +0200

    palettes: adding Online palette and consistently renaming 'color' to 'palette'

--- js/palettes.js
+++ js/palettes.js
@@ -1,6 +1,7 @@
 var Palettes = {
 
   palettes: {
+    // Standard palettes
     'blue': 'less/palettes/blue.css',
     'cyan': 'less/palettes/cyan.css',
     'green': 'less/palettes/green.css',
@@ -10,14 +11,16 @@ var Palettes = {
     'red': 'less/palettes/red.css',
     'white': 'less/palettes/white.css',
     'yellow': 'less/palettes/yellow.css',
+    // Special palettes
+    'online': Affiliation.org['online'].palette,
   },
 
-  getColor: function(color) {
-    if (this.palettes[color] != undefined) {
-      return this.palettes[color];
+  getPalette: function(palette) {
+    if (this.palettes[palette] != undefined) {
+      return this.palettes[palette];
     }
     else {
-      if (this.debug) console.log('ERROR: unsupported palette', color);
+      if (this.debug) console.log('ERROR: unsupported palette', palette);
       return this.palettes['blue'];
     }
   },
