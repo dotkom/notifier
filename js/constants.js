c8b904b8bfb5cd34576d223a3a0bec536f162633
Michael Johansen <michael@informatikk.org>
2013-05-07 13:02:08 +0200

    constants: increasing interval for checking bus times

--- js/constants.js
+++ js/constants.js
@@ -18,7 +18,7 @@ var UPDATE_MEETINGS_INTERVAL = 20; // recommended: 20
 var UPDATE_COFFEE_INTERVAL = 1; // recommended: 1
 var UPDATE_HOURS_INTERVAL = 60; // recommended: 60
 var UPDATE_CANTINAS_INTERVAL = 60; // recommended: 60
-var UPDATE_BUS_INTERVAL = 1; // recommended: 1
+var UPDATE_BUS_INTERVAL = 2; // recommended: 1
 var UPDATE_NEWS_INTERVAL = 20; // recommended: 20
 
 // OS detection
