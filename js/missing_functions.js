42553fc3900a84535a72909282c37d6809d32c3a
Michael Johansen <michael@informatikk.org>
2013-05-10 11:15:57 +0200

    missing_functions: adding hotfix for favorite bus lines, used by popup

--- js/missing_functions.js
+++ js/missing_functions.js
@@ -14,3 +14,32 @@ function isEmpty(v) {
   if (typeof v == 'boolean' && v == false) return true;
   return false;
 }
+
+function hotFixBusLines() {
+  // Active and inactive bus lines will sometimes be null (will show as NaN in options page)
+  // This isn't fixable yet because of some missing API features. For now we have to counter
+  // the effects by emptying the arrays for active and inactive bus lines, which will result
+  // in Notifier including all lines.
+  if (DEBUG) console.log('hotfixing!');
+  var active1 = JSON.parse(localStorage.firstBusActiveLines);
+  var inactive1 = JSON.parse(localStorage.firstBusInactiveLines);
+  var active2 = JSON.parse(localStorage.secondBusActiveLines);
+  var inactive2 = JSON.parse(localStorage.secondBusInactiveLines);
+  if (active1.indexOf(null) != -1) {
+    if (DEBUG) console.log('hotfixing activelines 1 ...');
+    localStorage.firstBusActiveLines = JSON.stringify([]);
+  }
+  if (inactive1.indexOf(null) != -1) {
+    if (DEBUG) console.log('hotfixing inactivelines 1 ...');
+    localStorage.firstBusInactiveLines = JSON.stringify([]);
+  }
+  if (active2.indexOf(null) != -1) {
+    if (DEBUG) console.log('hotfixing activelines 2 ...');
+    localStorage.secondBusActiveLines = JSON.stringify([]);
+  }
+  if (inactive2.indexOf(null) != -1) {
+    if (DEBUG) console.log('hotfixing inactivelines 2 ...');
+    localStorage.secondBusInactiveLines = JSON.stringify([]);
+  }
+  if (DEBUG) console.log('hotfixed!')
+}
