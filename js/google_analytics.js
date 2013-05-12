436d61741c69fb65248be474d0e3144ecb8ac4b6
Michael Johansen <michael@informatikk.org>
2013-05-12 04:46:51 +0200

    google_analytics: made sure page tracking is off when debugging - event tracking was already off, currently all analytics are off in DEBUG-mode

--- js/google_analytics.js
+++ js/google_analytics.js
@@ -1,25 +1,26 @@
+if (!DEBUG) {
+  // Tracking Basics (Asynchronous Syntax)
+  // https://developers.google.com/analytics/devguides/collection/gajs/
+  // Event Tracking - Web Tracking (ga.js):
+  // https://developers.google.com/analytics/devguides/collection/gajs/eventTrackerGuide
 
-// Tracking Basics (Asynchronous Syntax)
-// https://developers.google.com/analytics/devguides/collection/gajs/
-// Event Tracking - Web Tracking (ga.js):
-// https://developers.google.com/analytics/devguides/collection/gajs/eventTrackerGuide
+  // Event tracking:
+  // Params: _trackEvent, "event", "category", "action", "label", value (num), noninteraction (bool)
+  // Snippet: _gaq.push(['_trackEvent', 'subscription', 'click', image]);
 
-// Event tracking:
-// Params: _trackEvent, "event", "category", "action", "label", value (num), noninteraction (bool)
-// Snippet: _gaq.push(['_trackEvent', 'subscription', 'click', image]);
+  // Create google analytics array
+  var _gaq = _gaq || [];
 
-// Create google analytics array
-var _gaq = _gaq || [];
+  // Set account vars and set to track page views
+  _gaq.push(['_setAccount', 'UA-9905766-3']);
+  _gaq.push(['_trackPageview']);
 
-// Set account vars and set to track page views
-_gaq.push(['_setAccount', 'UA-9905766-3']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script');
-  ga.type = 'text/javascript';
-  ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(ga, s);
-})();
+  (function() {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+  })();
+}
