cd9204cae021909d81a6729f329ed409e7cebbff
Michael Johansen <michael@informatikk.org>
2013-05-15 19:15:36 +0200

    palettes: improving palette system so that it doesn't need manual references to special palettes

--- js/palettes.js
+++ js/palettes.js
@@ -10,18 +10,29 @@ var Palettes = {
     'purple': 'less/palettes/purple.css',
     'red': 'less/palettes/red.css',
     'yellow': 'less/palettes/yellow.css',
-    // Special palettes
-    'online': Affiliation.org['online'].palettePath,
   },
 
   get: function(palette) {
+    // Standard palette?
     if (this.palettes[palette] != undefined) {
       return this.palettes[palette];
     }
-    else {
-      if (this.debug) console.log('ERROR: unsupported palette', palette);
-      return this.palettes['blue'];
+
+    // A special palette belonging to a specific affiliation?
+    var affiliation = Affiliation.org[palette];
+    if (typeof affiliation != 'undefined') {
+      var path = Affiliation.org[palette].palettePath;
+      if (typeof path != 'undefined') {
+        return path;
+      }
+      else {
+        console.log('ERROR: a special palette has not yet been created for the affiliation', palette);
+        return this.palettes['blue'];
+      }
     }
+
+    console.log('ERROR: unsupported palette', palette);
+    return this.palettes['blue'];
   },
 
 }
