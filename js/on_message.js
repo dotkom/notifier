e55d3c9dbb5c3a0bc918886a39eac46d18d3d741
Michael Johansen <michael@informatikk.org>
2013-08-19 10:53:37 +0200

    on_message/on_website: several changes, introduced conversation handling - it looks like a lot, but really it's just two things, - the first is added conversation handling, like ordinary message passing, except the port is kept open for further communication - the second is that the resetUnreadCount function now works on all affiliation websites, not just Online's site - additionally the unread count doesn't just nullify entirely, it subtracts the correct amount of unread messages from the total count for both affiliations - example: with 7 unread items in which 3 are from Dusken.no, upon entering Dusken.no the badge will decrease to 4.

--- js/on_message.js
+++ js/on_message.js
@@ -1,35 +1,61 @@
 // Recieve all requests from parts of the extension that cannot access
 // localStorage or the rest of the codebase. Example: A content script.
+
+// onMessage for single messages
 function onMessage(request, sender, callback) {
-  if (request == 'resetCounterWhenOnWebsite' || request.action == 'resetCounterWhenOnWebsite') {
-    if (DEBUG) console.log('onRequest: resetCounterWhenOnWebsite');
-    localStorage.unreadCount = 0;
-    localStorage.viewedNewsList = localStorage.newsList;
-    Browser.setBadgeText('');
-  }
-  else if (request.action == 'getClickedCantina') {
-    if (DEBUG) console.log('onRequest: getClickedCantina');
+  // which cantina did the user just click in the popup?
+  // the content script wants to switch to that specific cantina
+  if (request.action == 'getClickedCantina') {
+    if (DEBUG) console.log('onMessage: getClickedCantina');
     var clickedCantina = localStorage.clickedCantina;
     if (typeof clickedCantina != 'undefined') {
       localStorage.removeItem('clickedCantina');
       callback(clickedCantina);
     }
   }
-  // else if (request.action == 'showCantina') {
-  //   callback(localStorage.showCantina);
-  // }
-  // else if (request.action == 'dismissSitNotice') {
-  //   localStorage.dismissedSitNotice = 'true';
-  //   callback(true);
-  // }
-  // else if (request.action == 'dismissedSitNotice') {
-  //   callback(localStorage.dismissedSitNotice);
-  // }
   else if (DEBUG) console.log('ERROR: unrecognized request');
 }
 
-// wire up the message listener function
-if (BROWSER == 'Chrome' || BROWSER == 'Opera')
-  if (typeof chrome.extension.onMessage != 'undefined')
+// onConnect for conversations
+function onConnect(port) {
+  // affiliationCounter is a conversation regarding resetting of the badge counter
+  // when a user visits the site of one of his/her affiliations
+  if (port.name == "affiliationCounter") {
+    if (DEBUG) console.log('onConnect: affiliationCounter');
+    port.onMessage.addListener(function(message) {
+      // first contact
+      if (message.getAffiliationWeb == '1' || message.getAffiliationWeb == '2') {
+        var number = message.getAffiliationWeb;
+        var affiliation = localStorage['affiliationKey' + number];
+        var web = Affiliation.org[affiliation].web;
+        port.postMessage({affiliationWeb: number + '@' + web});
+      }
+      // second contact
+      else if (typeof message.resetAffiliationCounter != 'undefined') {
+        var number = message.resetAffiliationCounter;
+        if (DEBUG) console.log('onMessage: resetAffiliationCounter for #' + number);
+        Browser.getBadgeText( function(badgeText) {
+          var badgeText = Number(badgeText);
+          if (!isNaN(badgeText)) {
+            var affiliationUnreadCount = localStorage['affiliationUnreadCount' + number];
+            var unreadCount = Number(affiliationUnreadCount);
+            var result = badgeText - unreadCount;
+            localStorage['affiliationUnreadCount' + number] = 0;
+            localStorage['affiliationViewedList' + number] = localStorage['affiliationNewsList' + number];
+            Browser.setBadgeText(result);
+          }
+        });
+      }
+    });
+  }
+  else if (DEBUG) console.log('WARNING: something tried to connect on port', port.name);
+}
+
+// wire up the message- and connect-listener functions
+if (BROWSER == 'Chrome' || BROWSER == 'Opera') {
+  if (typeof chrome.extension.onMessage != 'undefined') {
     chrome.extension.onMessage.addListener(onMessage);
+    chrome.runtime.onConnect.addListener(onConnect);
+  }
   else if (DEBUG) console.log('ERROR: old version of (chrom(e|ium)|opera), messaging API not supported');
+}
--- js/on_website.js
+++ js/on_website.js
@@ -10,23 +10,24 @@
 // might be an old, insecure website. Except, of course, if we are visiting
 // the Online website which is secured by our most paranoid guy, Rockj ;)
 
-var host = window.location.host;
+var host = window.location.href;
 
-if (host == 'online.ntnu.no') {
-  if (typeof chrome != "undefined") {
-    // Reset badge counter
-    chrome.extension.sendMessage({'action':'resetCounterWhenOnWebsite'});
-    // Hide Notifier install button
-    $('#install_notifier').hide();
-  }
+// online.ntnu.no
+
+if (host.indexOf('online.ntnu.no') != -1) {
+  // Hide Notifier install button
+  $('#install_notifier').hide();
 }
-else if (host == 'www.sit.no') {
+
+// sit.no
+
+if (host.indexOf('www.sit.no') != -1) {
   var callback = function(clickedCantina) {
     // Kick out SiTs own change function, which doesn't play
     // well with our more updated version of jQuery
     $('#displayWeek').unbind('change');
     // Rebind the cantina selector with SiT's own function
-    $('#displayWeek').change(function(){
+    $('#displayWeek').change( function() {
       var selectedDiner = $(this).val();
       $.ajax({
         url: 'ajaxdinner/get',
@@ -43,4 +44,35 @@ else if (host == 'www.sit.no') {
   if (typeof chrome != "undefined") {
     chrome.extension.sendMessage({'action':'getClickedCantina'}, callback);
   }
+}
+
+// all affiliations
+
+var resetCounter = function(affiliationNumber) {
+  var port = chrome.runtime.connect({name: "affiliationCounter"});
+  port.postMessage({getAffiliationWeb: affiliationNumber});
+  port.onMessage.addListener(function(msg) {
+    if (typeof msg.affiliationWeb != 'undefined') {
+      // Strip it down the bare essential web address for easy matching
+      // the format, for making communication really easy: 1@https://online.ntnu.no
+      var pieces = msg.affiliationWeb.split('@');
+      var number = pieces[0];
+      var web = pieces[1];
+      var strippedWeb = web.match(/(org\.ntnu\.no\/[\w\-]+)|([^w\/\.\s][\w\-]+\.[\w\.]+)/g);
+      if (strippedWeb != null) {
+        if (host.indexOf(strippedWeb) != -1) {
+          // The website has been recognized as an affiliation
+          port.postMessage({resetAffiliationCounter: String(number)});
+          // This resets badge counter for affiliation, example:
+          // If affiliation1 has 4 unread news items and affiliation2 has 3 unread news items,
+          // then upon a visit to affiliation1's website the counter will be decreased to 3.
+        }
+      }
+    }
+  });
+};
+// Try to reset the news counters
+if (typeof chrome != "undefined") {
+  resetCounter(1);
+  resetCounter(2);
 }
