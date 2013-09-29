bfeed50bec0dc8a8f5f36ac8b327676ce7500063
Michael Johansen <michael@informatikk.org>
2013-09-29 12:01:12 +0200

    oracle: added greetings

--- js/oracle.js
+++ js/oracle.js
@@ -28,6 +28,77 @@ var Oracle = {
     });
   },
 
+  greet: function() {
+    var greetings = [
+      // Spørsmål
+      'Når vil du ta bussen?',
+      'Bussorakelet trenger noe å gjøre, har du et spørsmål?',
+      'Har du et busspørsmål eller skal du bare lese litt tilfeldig tekst?',
+      'Lurer du på hvilken buss du skal ta?',
+      'Vil du ta bussen til Samfundet kanskje?',
+      'Vil du ta bussen til kjellerne på Moholt kanskje?',
+      'Har du et lite bussspørsmål?',
+      'Skal det være en buss?',
+      'Vil du ha en liten privat buss kanskje? En femseter?',
+      
+      // Statements
+      'Orakelet tar gjerne imot spørsmål.',
+      'Orakelet vet alt om hvilke busser som kjører.',
+      'Orakelet svarer på alle spørsmål om busser.',
+      'Spør, og du vil få svar.',
+      'Bussorakelet er sultent på spørsmål, eller kantinemat, alt ettersom.',
+      'Spør et busspørsmål her!',
+      'De som laget Notifier er ganske snille mot AtB.',
+      'Ta bussen til Tiller og kjøp no stæsj du trenger.',
+      'Ta bussen til Nidar og spis masse sjokolade.',
+      'Tralala, jeg kan synge en sang.',
+      'Buss her, buss der, buss overalt.',
+      'Ta bussen til campus.',
+
+      // Funfacts
+      'Bussorakelet ble utviklet på NTNU, spør om noe!',
+      'Tri M. Nguyen har laget APIet som brukes til sanntiden.',
+      'AtB har minibusser også.',
+      'UKA har en egen buss.',
+      'Flybussen går hvert 10. minutt.',
+      'Leddbusser har wifi.',
+      'AtB har også langdistansebusser, de er mørkegrønne.',
+      'Da Roy Sindre fant nøkkelen ble det plutselig en bussapp før AtB visste hva som skjedde.',
+      'En av bussjåførene er julenisse hver desember.',
+      'Både AtB og SiT liker Notifier.',
+      'Notifier fikk støtte for sanntidsbuss i versjon 1.3.0.',
+      'Tenk om Kantinelizzie hadde kjørt buss.',
+      'Hvis du har installert Notifier på forskjellige maskiner kan du ha forskjellige busstopp på hver.',
+      'Større busser, flere busser, raskere busser, AtB gjør alle tre på 5ern.',
+      'En gang gikk 5ern via Gryta og Lohove til Dragvoll.',
+      'Før i tiden møttes alle bussene i Munkegata/Dronningens gate.',
+      'AtB laget egen bussapp som het Bussøyet, men den var for sent etter Bartebuss.',
+      'Det finnes mange bussapper.',
+      'Bartebuss og ByBussen er de beste bussappene.',
+      'AtB sitt API er laget av italienere.',
+      'AtB sitt API har italienske metodenavn.',
+      'Bussene sjekker inn med GPS på hvert stopp, cirka.',
+      'Bussene som må kjøre langt mellom hvert stopp har upresis sanntid.',
+      'busskartet.no er ganske morsomt.',
+      'Han som laget busskartet.no må ha kost seg gløgg i hjæl med alle de små bussene sine.',
+
+      // Team Trafikk
+      'AtB er tusen ganger bedre enn Team Trafikk var.',
+      'I 2010 byttet Team Trafikk navn til AtB, og byttet eiere.',
+      'Team Trafikk sine busser var hvite, gamle og slitne.',
+      'Da Team Trafikk ble AtB ble alt mye bedre.',
+      'Da Team Trafikk ble AtB fikk de miljøvennlige busser.',
+      'Da Team Trafikk ble AtB fikk de grønnere busser.',
+      'Da Team Trafikk ble AtB fikk de mobillett.',
+      'Da Team Trafikk ble AtB fikk de nye kontorer.',
+      'Da Team Trafikk ble AtB fikk de API-er som man kunne hente data fra.',
+      'Da Team Trafikk ble AtB fant Roy Sindre en nøkkel til API-et, han spurte ikke om lov.',
+      'Åpne bussdata var veldig lurt av AtB.',
+    ];
+    var randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+    return randomGreeting;
+  },
+
   // Private functions, do not use externally
 
   shorten: function(answer) {
@@ -50,7 +121,7 @@ var Oracle = {
       pieces = pieces.slice(0, pieces.length-1);
     }
 
-    return pieces.join('. ');
+    return pieces.join('. ') + '.';
   },
 
 }
