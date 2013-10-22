fec5967abe7d312711da7d805048c2299d03eb56
Michael Johansen <michael@informatikk.org>
2013-10-22 14:35:42 +0200

    office: statuses and messages linked together

--- js/office.js
+++ js/office.js
@@ -4,24 +4,19 @@ var Office = {
 
   // Light limit, 0-860 is ON, 860-1023 is OFF
   lightLimit: 860,
-  // Basic
-  titles: {
-    'error': 'Oops',
-    'open': 'Åpent',
-    'closed': 'Lukket',
-    'meeting': 'Møte',
+  // Basic statuses have titles and messages (icons are fetched from affiliation)
+  statuses: {
+    'error': {title: 'Oops', message: 'Klarte ikke hente kontorstatus'},
+    'open': {title: 'Åpent', message: 'Gratis kaffe og te til alle!'},
+    'closed': {title: 'Lukket', message: 'Finn et komitemedlem for å åpne opp.'},
+    'meeting': {title: 'Møte', message: 'Kontoret er opptatt'}, // titled meetings get names from calendar entries
   },
-  // Foods
+  // Food statuses have titles and icons (messages exist as calendar titles)
   foods: {
     'waffle': {title: 'Vafler', icon: './img/icon-waffle.png'},
     'cake': {title: 'Kake', icon: './img/icon-cake.png'},
     'bun': {title: 'Boller', icon: './img/icon-bun.png'},
   },
-  // Messages
-  msgError: 'Klarte ikke hente kontorstatus',
-  msgOpen: 'Gratis kaffe og te til alle!',
-  msgClosed: 'Finn et komitemedlem for å åpne opp.',
-  msgUntitledMeeting: 'Kontoret er opptatt', // titled events get names from calendar entries
 
   get: function(callback) {
     if (callback == undefined) {
@@ -72,25 +67,26 @@ var Office = {
 
         // empty meeting title?
         if (status == 'meeting' && title == '')
-          title = self.msgUntitledMeeting;
+          title = self.statuses['meeting'].message;
 
         // set the status from fetched data
         switch(status) {
           
           case 'free': callback('free'); break;
+
+          case 'meeting': callback('meeting', self.statuses['meeting'].title, title); break;
           
-          case 'meeting': callback('meeting', self.titles.meeting, title); break;
           case 'waffle': callback('waffle', self.foods.waffle.title, title); break;
           case 'cake': callback('cake', self.foods.cake.title, title); break;
           case 'bun': callback('bun', self.foods.bun.title, title); break;
           
           case 'error':
-          default: callback('error', self.titles.error, self.msgError);
+          default: callback('error', self.statuses['error'].title, self.statuses['error'].message);
         }
       },
       error: function(jqXHR, text, err) {
         if (self.debug) console.log('ERROR: Failed to get event data.');
-        callback('error', self.titles.error, self.msgError);
+        callback('error', self.statuses['error'].title, self.statuses['error'].message);
       },
     });
   },
@@ -109,17 +105,17 @@ var Office = {
       url: lightApi,
       success: function(data) {
         if (data > self.lightLimit) {
-          if (self.debug) console.log('Office:\n- status is', status, '\n- title is', self.titles.closed, '\n- message is', self.msgClosed);
-          callback('closed', self.titles.closed, self.msgClosed);
+          if (self.debug) console.log('Office:\n- status is closed\n- title is', self.statuses['closed'].title, '\n- message is', self.statuses['closed'].message);
+          callback('closed', self.statuses['closed'].title, self.statuses['closed'].message);
         }
         else {
-          if (self.debug) console.log('Office:\n- status is', status, '\n- title is', self.titles.open, '\n- message is', self.msgOpen);
-          callback('open', self.titles.open, self.msgOpen);
+          if (self.debug) console.log('Office:\n- status is open\n- title is', self.statuses['open'].title, '\n- message is', self.statuses['open'].message);
+          callback('open', self.statuses['open'].title, self.statuses['open'].message);
         }
       },
       error: function(jqXHR, err) {
         if (self.debug) console.log('ERROR: Failed to get light data.');
-        callback('error', self.titles.error, self.msgError);
+        callback('error', self.statuses['error'].title, self.statuses['error'].message);
       },
     });
   },
