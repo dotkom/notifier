5469e212347b8fd9dd734e03b18596ac5dae6b09
Michael Johansen <michael@informatikk.org>
2014-01-25 09:35:59 +0100

    prototypes: new missing function

--- js/missing_prototypes.js
+++ js/missing_prototypes.js
@@ -7,6 +7,10 @@ String.prototype.capitalize = function() {
   return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
-String.prototype.startsWith = function (string) {
-  return this.indexOf(string) == 0;
+String.prototype.startsWith = function (prefix) {
+  return this.indexOf(prefix) == 0;
 }
+
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
