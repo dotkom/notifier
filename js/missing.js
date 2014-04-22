265d0e53df0b87b26a9ca5a1aa82559392395cf1
Michael Johansen <michael@informatikk.org>
2014-04-22 20:33:02 +0200

    missing: added getWeekNumber (moved from calendar.js)

--- js/missing.js
+++ js/missing.js
@@ -33,3 +33,10 @@ String.prototype.startsWith = function (prefix) {
 String.prototype.endsWith = function(suffix) {
     return this.indexOf(suffix, this.length - suffix.length) !== -1;
 }
+
+Date.prototype.getWeekNumber = function () {
+    var d = new Date(+this);
+    d.setHours(0,0,0);
+    d.setDate(d.getDate()+4-(d.getDay()||7));
+    return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7);
+};
