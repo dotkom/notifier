cd3bcb662d3283ceb7fa2a459029db5a893d9959
Michael Johansen <michael@informatikk.org>
2014-04-30 15:54:30 +0200

    missing: added array.contains function

--- js/missing.js
+++ js/missing.js
@@ -3,7 +3,7 @@
 
 function isNumber(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
-}
+};
 
 function isEmpty(v) {
   if (typeof v == 'undefined' || v == null) return true;
@@ -11,32 +11,36 @@ function isEmpty(v) {
   if (typeof v == 'number' && (isNaN(v) || v == 0)) return true;
   if (typeof v == 'boolean' && v == false) return true;
   return false;
-}
+};
 
 function stacktrace() {
   var e = new Error('dummy');
   var stack = e.stack.replace(/^[^\(]+?[\n$]/gm, '').replace(/^\s+at\s+/gm, '').replace(/^Object.<anonymous>\s*\(/gm, '{anonymous}()@').split('\n');
   stack = stack.slice(1);
   console.log(stack);
-}
+};
 
 // Small prototype library for Online Notifier
 
-String.prototype.capitalize = function() {
-  return this.charAt(0).toUpperCase() + this.slice(1);
-}
-
-String.prototype.startsWith = function (prefix) {
-  return this.indexOf(prefix) == 0;
-}
-
-String.prototype.endsWith = function(suffix) {
-    return this.indexOf(suffix, this.length - suffix.length) !== -1;
-}
+Array.prototype.contains = function(value) {
+  return (this.indexOf(value) > -1);
+};
 
 Date.prototype.getWeekNumber = function () {
     var d = new Date(+this);
     d.setHours(0,0,0);
     d.setDate(d.getDate()+4-(d.getDay()||7));
     return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7);
+};
+
+String.prototype.capitalize = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+String.prototype.startsWith = function (prefix) {
+  return this.indexOf(prefix) == 0;
+};
+
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
 };
