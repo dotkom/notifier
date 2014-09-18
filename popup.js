9034ad9b117bdcd90711ae834aa3754912d04f28
Shade4e <petterrostrup@gmail.com>
2014-09-18 16:03:27 +0200

    Improved the konami easter egg

--- popup.js
+++ popup.js
@@ -791,23 +791,12 @@ $(document).ready(function() {
     callback: function() {
       Analytics.trackEvent('toggleKonami');
       // Animate background
-      $('head').append(([
-        '<style type="text/css">',
-          '@-webkit-keyframes adjustHue {',
-          '0% { -webkit-filter: hue-rotate(0deg); }',
-          '10% { -webkit-filter: hue-rotate(36deg); }',
-          '20% { -webkit-filter: hue-rotate(72deg); }',
-          '30% { -webkit-filter: hue-rotate(108deg); }',
-          '40% { -webkit-filter: hue-rotate(144deg); }',
-          '50% { -webkit-filter: hue-rotate(180deg); }',
-          '60% { -webkit-filter: hue-rotate(216deg); }',
-          '70% { -webkit-filter: hue-rotate(252deg); }',
-          '80% { -webkit-filter: hue-rotate(288deg); }',
-          '90% { -webkit-filter: hue-rotate(324deg); }',
-          '100% { -webkit-filter: hue-rotate(360deg); }',
-        '}</style>'
-        ]).join('\n'));
-      $('#background').attr('style','-webkit-animation:adjustHue 10s alternate infinite;');
+      //Sets the background to awesomeness
+      //var rick = '<iframe id="konamiVid" width="532" height="600" src="http://www.youtube.com/embed/qyXTgqJtoGM?autoplay=1&showinfo=0" frameborder="0"></iframe>'
+      $('#bus').css('background-color', 'transparent');
+      //$('#background').append(rick);
+      var trollvid = '<iframe width="532" height="600" src="http://www.youtube.com/embed/eh7lp9umG2I?autoplay=1&loop=1&showinfo=0&autohide=1" frameborder="0" allowfullscreen></iframe>'
+      $("#background").html(trollvid);
     },
   });
 
