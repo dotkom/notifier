929d30806cf960e8a0d2877925143943fe8cfdcd
Michael Johansen <michael@informatikk.org>
2014-10-18 17:30:00 +0200

    ajaxer: reduced timeout, punishing slow sites

--- js/ajaxer.js
+++ js/ajaxer.js
@@ -3,7 +3,7 @@ Ajaxer = {
 
   // Ajax setup for all requests, this snippet is added to jQuery setup at the end of this file
   ajaxSetup: {
-    timeout: 12000, // 10s+, the sole reason for this is Gemini's ridiculously slow and large news feed
+    timeout: 6000, // anything longer is too long
     cache: false, // this little sentence killed a lot of little bugs that was actually one big bug
   },
 
