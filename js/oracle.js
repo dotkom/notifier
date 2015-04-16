f555d6f0b496f176e0ce63b291319fafb8a113dc
Michael Johansen <michael@informatikk.org>
2015-04-16 04:03:36 +0200

    oracle: minor wording change

--- js/oracle.js
+++ js/oracle.js
@@ -515,7 +515,7 @@ var Oracle = {
 
   // getEndStop: function(line, fromStop, callback) {
   //   if (typeof callback === 'undefined') {
-  //     console.error('Callback is required. In the callback you should insert the results into the DOM.');
+  //     console.error('Callback is required');
   //     return;
   //   }
   //   if (typeof line !== 'number') {
