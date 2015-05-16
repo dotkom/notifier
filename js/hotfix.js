aa20952f8c8c27650a6805261569993abd617890
Michael Johansen <michael@informatikk.org>
2015-05-16 21:20:16 +0200

    hotfix: updating localstorage variable names

--- js/hotfix.js
+++ js/hotfix.js
@@ -1,33 +1,21 @@
 "use strict";
 
-// HOTFIXING CANTINA MENU
-// a cantina was removed
-// REMOVE AFTER MAY 2015
-
-if (localStorage.leftCantina === 'ranheimsveien')
-	localStorage.leftCantina = 'tungasletta';
-if (localStorage.rightCantina === 'ranheimsveien')
-	localStorage.rightCantina = 'tungasletta';
-
-// REMOVING PINK PALETTE
-// it never really fit, was also the least used one (0.70%)
-// REMOVE AFTER MAY 2015
-
-if (localStorage.affiliationPalette === 'pink')
-	localStorage.affiliationPalette = 'purple';
-
-// RENAMING LOCALSTORAGE VARS
-// it was an awful naming convention, numbering is better
-// REMOVE AFTER MAY 2015
-
-if (localStorage.leftCantina)
-	localStorage.cantina1 = localStorage.leftCantina;
-if (localStorage.rightCantina)
-	localStorage.cantina2 = localStorage.rightCantina;
-
 // RENAMING LOCALSTORAGE VARS
 // conforming to new convention in Notiwire
 // REMOVE AFTER JUNE 2015
 
 if (localStorage.showOffice)
 	localStorage.showStatus = localStorage.showOffice;
+
+// RENAMING LOCALSTORAGE VARS
+// deprecating ancient naming scheme
+// REMOVE AFTER AUGUST 2015
+
+if (localStorage.affiliationFeedItems1) {
+	localStorage.affiliationNews1 = localStorage.affiliationFeedItems1;
+	localStorage.removeItem('affiliationFeedItems1');
+}
+if (localStorage.affiliationFeedItems2) {
+	localStorage.affiliationNews2 = localStorage.affiliationFeedItems2;
+	localStorage.removeItem('affiliationFeedItems2');
+}
