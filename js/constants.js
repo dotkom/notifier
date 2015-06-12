317ab41811a0bd8b954c9ecc5015ef565b016677
Michael Johansen <michael@informatikk.org>
2015-06-12 19:36:11 +0200

    constants: now actual constants rather than vars, thanks to ES6 and evergreen browsers

--- js/constants.js
+++ js/constants.js
@@ -1,11 +1,11 @@
 "use strict";
 
-var DEBUG = 1;
+//
+// Debugging
+//
 
-// Production detection
-if (Browser.inProduction()) {
-	DEBUG = 0;
-}
+// Declare DEBUG constant, but be sure we aren't in production
+const DEBUG = Browser.inProduction() ? false : true;
 
 // Disable logging if in production
 if (!DEBUG) {
@@ -16,23 +16,27 @@ if (!DEBUG) {
 	window.console.error = function(){};
 }
 
+//
+// All other constants
+//
+
 // API servers
-var API_SERVER_1 = 'http://passoa.online.ntnu.no/api/';
-var API_SERVER_2 = 'http://online.duvholt.net/api/';
+const API_SERVER_1 = 'http://passoa.online.ntnu.no/api/';
+const API_SERVER_2 = 'http://online.duvholt.net/api/';
 
 // Loops & intervals
-var BACKGROUND_LOOP = 60000; // 60s
-var BACKGROUND_LOOP_DEBUG = 5000; // 5s, respond fairly quickly for us developers
-var PAGE_LOOP = 10000; // 10s
-var PAGE_LOOP_DEBUG = 5000; // 5s
-var ONLINE_MESSAGE = '\nNow online, run mainloop\n';
-var OFFLINE_MESSAGE = '\nNow offline, stop execution\n';
+const BACKGROUND_LOOP = 60000; // 60s
+const BACKGROUND_LOOP_DEBUG = 5000; // 5s, respond fairly quickly for us developers
+const PAGE_LOOP = 10000; // 10s
+const PAGE_LOOP_DEBUG = 5000; // 5s
+const ONLINE_MESSAGE = '\nNow online, run mainloop\n';
+const OFFLINE_MESSAGE = '\nNow offline, stop execution\n';
 
 // Update stuff at every X intervals
-var UPDATE_AFFILIATION_INTERVAL = 1; // recommended: 1
-var UPDATE_CANTINAS_INTERVAL = 60; // recommended: 60
-var UPDATE_BUS_INTERVAL = 2; // recommended: 1
-var UPDATE_NEWS_INTERVAL = 20; // recommended: 20
+const UPDATE_AFFILIATION_INTERVAL = 1; // recommended: 1
+const UPDATE_CANTINAS_INTERVAL = 60; // recommended: 60
+const UPDATE_BUS_INTERVAL = 2; // recommended: 1
+const UPDATE_NEWS_INTERVAL = 20; // recommended: 20
 
 // Hard totals
-var MEME_AMOUNT = 30;
+const MEME_AMOUNT = 30;
