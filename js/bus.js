80f1a819ca295bfc2478b33e4417a6f730a3089f
Michael Johansen <michael@informatikk.org>
2016-02-07 17:31:18 +0100

    bus: fixed a bug where a passed bus would show all realtime data as invalid

--- js/bus.js
+++ js/bus.js
@@ -7,7 +7,6 @@ var Bus = {
   msgConnectionError: 'Tilkoblingsfeil',
   msgInvalidDirection: 'Ugyldig retning',
   msgKeyExpired: 'Nøkkelen vår har utløpt, trenger oppdatering',
-  msgOldData: 'Mottok utdatert sanntidsdata',
 
   // Public functions
 
@@ -26,11 +25,12 @@ var Bus = {
 
   calculateUrgency: function(timeString) {
     var urgencyColors = ['#ad0000', '#c80909', '#d91a1a', '#eb3131', '#fd4242', '#e56565', '#d97575', '#ce7f7f', '#c58e8e', '#b68f8e', '#a99291', '#9c9595'];
-    // var urgencyColors = ['#950000', '#a10f0f', '#b12222', '#b53535', '#b74b4b', '#b46161', '#a16767', '#906868', '#7e6969', '#6d6363', '#636262', '#4f4f4f'];
-    // var urgencyColors = ['#df0101', '#ff0000', '#fe2e2e', '#ff5555', '#ff8686', '#ffadad', '#ffd1d1', '#f8e0e0', '#f0e2e2', '#eeeeee', '#cccccc', '#bbbbbb'];
     timeString = timeString.replace('ca ', '');
     timeString = timeString.replace(' min', '');
-    if (timeString === 'nå') {
+    if (timeString.match(/kjørt/) !== null) {
+      return '#ff69b4';
+    }
+    else if (timeString.match(/nå/) !== null) {
       return urgencyColors[0];
     }
     else if (timeString.match(/[0-9]{2}:[0-9]{2}/g)) {
@@ -84,12 +84,6 @@ var Bus = {
       var destination = departures[i].d.trim();
       var calculatedTime = this.calculateTime(time, isRealtime);
 
-      // Callback and return if realtime data is old (bus info from this morning rather than now)
-      if (calculatedTime === this.msgOldData) {
-        callback(this.msgOldData);
-        return;
-      }
-
       // Add destination and departure
       lines.destination.push(line + ' ' + destination);
       lines.departures.push(calculatedTime);
@@ -102,7 +96,6 @@ var Bus = {
     callback(lines);
   },
 
-  // If calculateTime returns this.msgOldData then we have old realtime data from this morning rather than now
   calculateTime: function(time, isRealtime) {
     // We only need the time, the date is always today
     // Format is "10.11.2012 12:07"
@@ -126,7 +119,7 @@ var Bus = {
     // Create a proper time string from all the minutes
     var calculatedTime;
     if (diff < -1)
-      calculatedTime = this.msgOldData; // This is an error
+      calculatedTime = isRealtime ? 'kjørt' : 'ca kjørt';
     else if (-1 <= diff && diff <= 0)
       calculatedTime = isRealtime ? 'nå' : 'ca nå';
     else if (1 <= diff && diff <= 59)
